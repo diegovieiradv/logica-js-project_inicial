d20cacf49777fe0f625bc27fc5a57c8d29f47492
Diego Vieira <dvdiego87@gmail.com>
2025-07-25 02:52:55 -0300

    atualizei project

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ alert("Seja bem-vindo ao nosso jogo do número secreto!");
 
 let numeroSecreto = 10;
 
-prompt("Digite um número entre 0 e 20");
+let chute = Number(prompt("Digite um número entre 0 e 10"));
+
+if (chute === numeroSecreto) {
+    alert(`Parabéns! Você acertou, o número secreto é ${numeroSecreto}`);
+}
+else {
+    alert("Que pena! Você errou.");
+    }

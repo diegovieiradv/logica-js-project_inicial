e0adf743eccb429927da7d42467afd85fff5ce1c
diegovieiradv <dvdiego87@gmail.com>
2025-07-29 01:17:40 -0300

    atualizei exercicio

--- app.js
+++ app.js
@@ -9,15 +9,28 @@ while (chute != numeroSecreto) {
   chute = Number(prompt("Digite um número entre 0 e 10"));
 
   if (chute == numeroSecreto) {
-    alert(
-      `Parabéns! Você acertou, o número secreto é ${numeroSecreto} com ${tentativas} tentativas.`
-    );
+    break;
   } else {
     if (chute > numeroSecreto) {
       alert(`O numero secreto é menor que ${chute}`);
     } else {
       alert(`O numero secreto é maior que ${chute}`);
     }
+    tentativas++;
   }
-  tentativas++;
 }
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(
+  `Você acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`
+);
+
+// e a mesma coisa que esse codigo abaixo
+/*if (tentativas > 1) {
+  alert(
+    `Parabéns! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas.`
+  );
+} else {
+  alert(
+    `Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativa!`
+  );
+}*/

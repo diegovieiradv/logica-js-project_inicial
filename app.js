4684396cc813edc72254ccb2db0e8c43e85473d5
diegovieiradv <dvdiego87@gmail.com>
2025-07-29 00:26:59 -0300

    atualizei exercicio

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 alert("Seja bem-vindo ao nosso jogo do número secreto!");
 
 let numeroSecreto = 10;
+let tentativas = 1;
 
-let chute = Number(prompt("Digite um número entre 0 e 10"));
+let chute;
 
-if (chute === numeroSecreto) {
-    alert(`Parabéns! Você acertou, o número secreto é ${numeroSecreto}`);
-}
-else {
-    alert("Que pena! Você errou.");
+while (chute != numeroSecreto) {
+  chute = Number(prompt("Digite um número entre 0 e 10"));
+
+  if (chute == numeroSecreto) {
+    alert(
+      `Parabéns! Você acertou, o número secreto é ${numeroSecreto} com ${tentativas} tentativas.`
+    );
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O numero secreto é menor que ${chute}`);
+    } else {
+      alert(`O numero secreto é maior que ${chute}`);
     }
+  }
+  tentativas++;
+}
